7967196a32a6b6b194c50ab345f04a47dc6a730c
ndadashi62 <nasim.dadashi@gmail.com>
2021-02-09 10:55:38 -0500

    result activitye has been made

---
+++ app/src/main/java/com/example/concordiaq/ReslutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.concordiaq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReslutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reslut)
+    }
+}
