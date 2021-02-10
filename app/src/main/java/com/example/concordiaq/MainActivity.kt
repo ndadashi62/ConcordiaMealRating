926e059af60ea919d1a3797774847e398bf47a62
ndadashi62 <nasim.dadashi@gmail.com>
2021-02-10 10:29:15 -0500

    main layer has been added

--- app/src/main/java/com/example/concordiaq/MainActivity.kt
+++ app/src/main/java/com/example/concordiaq/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.concordiaq
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RatingBar.OnRatingBarChangeListener
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
     }
 }
